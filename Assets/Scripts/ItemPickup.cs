50122ef49689a1498ce509333064b0b89929fc27
Demirhan-Yusuf <72434470+Demirhan-Yusuf@users.noreply.github.com>
2023-04-26 19:44:01 +0300

    push

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+
+public class ItemPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string playerTag = "Player";
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag(playerTag))
+        {
+            PickUpItem();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PickUpItem()
     {
-        
+        // Handle item pickup functionality here
+        // For example, you could increase the player's score, health, etc.
+
+        Destroy(gameObject); // Destroy the item after it's picked up
     }
 }
+
